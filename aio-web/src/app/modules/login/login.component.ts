12597d200d9529220e818da2868c3bb5467de906
saurabh sharma <saurabhsharma24888@gmail.com>
2025-07-05 16:53:14 +0530

    13 to 14 step 1

--- aio-web/src/app/modules/login/login.component.ts
+++ aio-web/src/app/modules/login/login.component.ts
@@ -1,6 +1,6 @@
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { UntypedFormControl } from '@angular/forms';
 import { User } from 'src/app/modals/user.modal';
 import { UserApiUrlsService } from 'src/app/services/constant-services/user-api.service';
 import { Router } from '@angular/router';
@@ -12,28 +12,28 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   hide = true;
-  registrationForm = new FormGroup({
-    email: new FormControl(null),
-    password: new FormControl(null),
-    confirmPassword: new FormControl(null)
+  registrationForm = new UntypedFormGroup({
+    email: new UntypedFormControl(null),
+    password: new UntypedFormControl(null),
+    confirmPassword: new UntypedFormControl(null)
 
   });
   isSubmitted = false;
 
   constructor(
     private _userApiService: UserApiUrlsService,
-    private _formBuilder: FormBuilder,
+    private _formBuilder: UntypedFormBuilder,
     private _router: Router
   ) { }
   user = new User("", "", 0, 0);
 
-  name = new FormControl('');
+  name = new UntypedFormControl('');
 
 
   ngOnInit(): void {
-    this.registrationForm = new FormGroup({
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
+    this.registrationForm = new UntypedFormGroup({
+      email: new UntypedFormControl('', Validators.required),
+      password: new UntypedFormControl('', Validators.required)
     })
 
   }
