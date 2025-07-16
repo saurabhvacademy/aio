ffa91830a5e77fe140ff80bf6709e92f519d9577
saurabhsharma <saurabhsharma24888@gmail.com>
2025-07-17 00:26:22 +0530

    auth guard added

--- aio-web/src/app/services/auth-services/auth.service.ts
+++ aio-web/src/app/services/auth-services/auth.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   public login(accessToken:string){
     localStorage.setItem('ACCESS_TOKEN', accessToken);
   }
 
   public isLoggedIn(){
+    _router.navigate(['/register']);
     return localStorage.getItem('ACCESS_TOKEN') !== null;
 
   }
