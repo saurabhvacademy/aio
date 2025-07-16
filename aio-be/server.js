c6e5b83cff23327ef6f97962f582c36017965975
saurabhsharma <saurabhsharma24888@gmail.com>
2025-07-17 01:40:29 +0530

    cors allowed

--- aio-be/server.js
+++ aio-be/server.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
+app.use(cors({
+  origin: '*', // Or your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
